deedf51fbcd3c2a9a57d9cc6692521d1da93f11c
Amina Rakhbari <amina.rakhbari@yandex.ru>
2025-11-11 21:27:15 +0300

    Добавила домены в корсы

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,13 +7,25 @@ const { PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const allowedOrigins = [
+    `${httpLocalhost}:8081`,
+    'http://a-rakhbari.nomorepartiessbs.ru',
+    'https://a-rakhbari.nomorepartiessbs.ru',
+  ];
   
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.enableCors({
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
     methods: httpCorsMethods,
-    origin: `${httpLocalhost}:8081`,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(null, true); // Временно разрешаем все для отладки
+      }
+    },
   });
   await app.listen(PORT);
 }
